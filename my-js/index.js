bdac4121734d204e0acde023aad66c5176ce5149
Sazzad Hossain <hossain.sakib26@hotmail.com>
2022-11-01 11:42:54 +0600

    update nave complete but have a little isue with hiding

--- my-js/index.js
+++ my-js/index.js
@@ -1,11 +1,21 @@
+function changeNav(){
+    let naveKeeperEll = document.getElementById('side-nav-keeper');
+    let mobileNavEll = document.getElementById('mobile-nav');
+    const naveKeeper = naveKeeperEll.clientWidth;
+    const mobileNav = mobileNavEll.clientWidth;
+    console.log(naveKeeper, mobileNav);
 
-function changeSidNav() {
-    var selectedElement = document.getElementById('mobile-nav');
-    var sideNavWidth = selectedElement.clientWidth;
+    ((naveKeeper > 0) && (mobileNav > 0)) ? closeSideNav(naveKeeperEll, mobileNavEll) : openSideNav(naveKeeperEll, mobileNavEll);
 
-    if (sideNavWidth > 0) {
-        selectedElement.style.width = "0";
-    }else {
-        selectedElement.style.width = "65%";
-    }
+}
+function openSideNav(navKeeperEll, mobileNavEll){
+    navKeeperEll.style.width = "100%";
+    mobileNavEll.style.width = "65%";
+}
+
+function closeSideNav(navKeeperEll, mobileNavEll){
+    navKeeperEll.style.width = "0";
+    // navKeeperEll.style.right = "-1%";
+    mobileNavEll.style.width = "0";
+    // mobileNavEll.style.right = "-1%";
 }
